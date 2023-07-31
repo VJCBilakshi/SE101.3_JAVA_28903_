202247b96b1d433e750e55cc1249605c0873a6a1
VJCBilakshi <136309663+VJCBilakshi@users.noreply.github.com>
2023-07-31 19:23:45 +0530

    Upload Java Practical 1

--- Practical 01/practical 1 codes/P02/p02.java
+++ Practical 01/practical 1 codes/P02/p02.java
@@ -2,8 +2,7 @@ public class Main
 {
 	public static void main(String [] args)
 	{
-		System.out.println("Janani Chamodya");
-		System.out.println("BSc (Hons) in Computer Networks");
+		System.out.println("Jeneesha Priyadarshani");
 		System.out.println("BSc honors Computer Science");
 	}
 }
